e74a58fb113aa63be0d062dadb80db294feb3895
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-30 15:44:21 +0100

    login + register views and routes

--- src/routing/router.jsx
+++ src/routing/router.jsx
@@ -4,6 +4,8 @@ import Homepage from "../views/Homepage";
 import Posts from "../views/Posts";
 import { getAllPostsLoader, getSinglePostLoader } from "./loaders";
 import DetailView from "../views/DetailView";
+import Login from "../views/Login";
+import Register from "../views/Register";
 
 const router = createBrowserRouter([
     {
@@ -23,6 +25,14 @@ const router = createBrowserRouter([
                 path: '/posts/detail/:id',
                 Component: DetailView,
                 loader: getSinglePostLoader
+            },
+            {
+                path: '/login',
+                Component: Login
+            },
+            {
+                path: '/register',
+                Component: Register
             }
         ]
     }
