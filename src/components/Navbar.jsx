8fc83dfdbe03d6e76efc023ed8887fabd23ee4b1
Ale-1407 <alessiacarrera07@gmail.com>
2025-12-04 00:41:13 +0100

    useScroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { useContext } from "react";
 import { Link } from "react-router";
 import { UserContext } from "../context/UserContext";
+import useScroll from "../hooks/useScroll";
 
 
 export default function Navbar(){
 
     const { user, logout } = useContext(UserContext);
 
+    const [scrolled, scrollY] = useScroll();
+
     return (
-        <nav className="custom-nav p-4 d-flex justify-content-between">
+        <nav ref={scrolled} className={scrollY > 0 ? 'new-custom-nav nav' : 'custom-nav nav'} >
             <ul>
                 <li>
                    <Link to={'/'}>Home</Link>
